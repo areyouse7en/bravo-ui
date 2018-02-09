af1566d6606c1a0b34d3c0da6598587da6a7df56
seven <373084212@qq.com>
2018-02-09 09:58:16 +0800

    config

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
-# will-ui
+# bravo-ui
 A set of mobile components for Vue2. 
+
+## Build Setup
+
+``` bash
+yarn
+
+yarn serve
+```
--- vue.config.js
+++ vue.config.js
@@ -2,5 +2,8 @@ module.exports = {
   lintOnSave: true,
   configureWebpack: {
     entry: './examples/main'
+  },
+  devServer: {
+    port: 3000
   }
 }
