74c9577cc1203e09e72a0d874a559c582412f302
seven <373084212@qq.com>
2018-02-09 17:22:52 +0800

    feat:styles path

--- examples/App.vue
+++ examples/App.vue
@@ -1,28 +1,14 @@
-<template>
-  <div id="app">
-    <img src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+<template lang="pug">
+div
+  Icon(type="camera",color="blue")
+  Icon(type="good")
+  Icon(type="code")
+  Icon(type="apparel")
+  Icon(type="add")
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'app',
-  components: {
-    HelloWorld
-  }
-}
+  name: "app"
+};
 </script>
-
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
